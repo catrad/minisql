5b9f49e5e651c8d8fb0cc8c4dcc9bbdf2850fc73
catrad <61656265+catrad@users.noreply.github.com>
2021-05-11 17:37:40 +0300

    Add files via upload

---
+++ minisqlold.py
@@ -0,0 +1,84 @@
+# MINISQL V 1.0. OLD VERSION
+def add(baza, yach, value):
+	import sqlite3
+	import traceback
+	import sys
+
+	try:
+	    sqlite_connection = sqlite3.connect(baza[0])
+	    cursor = sqlite_connection.cursor()
+	    sqlite_insert_query = """INSERT INTO %s
+	                          %s VALUES  %s""" % (baza[1], yach, value)
+
+	    count = cursor.execute(sqlite_insert_query)
+	    sqlite_connection.commit()
+	    cursor.close()
+
+	except sqlite3.Error as error:
+	    print("Не удалось вставить данные в таблицу sqlite")
+	    print("Класс исключения: ", error.__class__)
+	    print("Исключение", error.args)
+	    print("Печать подробноcтей исключения SQLite: ")
+	    exc_type, exc_value, exc_tb = sys.exc_info()
+	    print(traceback.format_exception(exc_type, exc_value, exc_tb))
+	finally:
+	    if (sqlite_connection):
+	        sqlite_connection.close()
+
+def read(baza):
+	import sqlite3
+	sqlite_connection = sqlite3.connect(baza[0])
+	cursor = sqlite_connection.cursor()
+	cursor.execute(f"SELECT * FROM {baza[1]}")
+	results = cursor.fetchall() 
+	sqlite_connection.commit()
+	cursor.close()
+	return results
+
+def get(baza, yach, key, key1):
+	import sqlite3
+	sqlite_connection = sqlite3.connect(baza[0])
+	cursor = sqlite_connection.cursor()
+	cursor.execute(f"SELECT {yach} FROM {baza[1]} WHERE {key}={key1}")
+	results = cursor.fetchall() 
+	sqlite_connection.commit()
+	cursor.close()
+	return results[0][0]
+
+def edit(baza, yach, edit_to, key, key1):
+	import sqlite3
+	sqlite_connection = sqlite3.connect(baza[0])
+	cursor = sqlite_connection.cursor()
+	sql_update_query = """Update %s set %s = ? where %s = ?""" % (baza[1], yach, key)
+	data = (edit_to, key1)
+	cursor.execute(sql_update_query, data)
+	sqlite_connection.commit()
+	return True
+	cursor.close()
+	if (sqlite_connection):
+	        sqlite_connection.close()
+
+def iget(baza, yach, key, key1, key2, key3):
+	import sqlite3
+	sqlite_connection = sqlite3.connect(baza[0])
+	cursor = sqlite_connection.cursor()
+	cursor.execute(f"SELECT {yach} FROM {baza[1]} WHERE {key}={key1} AND {key2}={key3}")
+	results = cursor.fetchall() 
+	sqlite_connection.commit()
+	cursor.close()
+	return results[0][0]
+
+def iedit(baza, yach, edit_to, key, key1, key2, key3):
+	import sqlite3
+	sqlite_connection = sqlite3.connect(baza[0])
+	cursor = sqlite_connection.cursor()
+	sql_update_query = """Update %s set %s = ? where %s = ? and %s = ?""" % (baza[1], yach, key, key2)
+	data = (edit_to, key1, key3)
+	cursor.execute(sql_update_query, data)
+	sqlite_connection.commit()
+	return True
+	cursor.close()
+	if (sqlite_connection):
+	        sqlite_connection.close()
+
+
